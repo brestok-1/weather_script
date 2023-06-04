085f7b2a67adb642e1f65af5d77b01618e1185f3
maksim <6234513@gmail.com>
2023-06-04 03:07:43 +0300

    the type of coordinates was defined

---
+++ gps_coordinates.py
@@ -0,0 +1,51 @@
+from typing import NamedTuple
+
+
+# from dataclasses import dataclass
+
+
+class Coordinates(NamedTuple):
+    latitude: float
+    longitude: float
+
+
+def get_gps_coordinates() -> Coordinates:  # -> tuple[float, float]:  Defines what type of data the function should return
+    """Returns current coordinates """
+    return Coordinates(latitude=0.0, longitude=0.0)
+
+
+# 2)
+# def get_gps_coordinates() -> dict[Literal['latitude'] | Literal['longitude'], float]
+#     """Returns current coordinates using"""
+#     return {'latitude' : 10.0, 'longitude' : 15.0}
+# coordinates = get_gps_coordinates()
+# print(coordinates['latitude'])
+
+# 3) There is no real usage
+# class Coordinates(TypedDict):
+#     latitude: float
+#     longitude: float
+#
+#
+# def get_gps_coordinates() -> Coordinates:
+#     """Returns current coordinates using"""
+#     return Coordinates(**{'latitude': 10.0, 'longitude': 15.0})
+#
+#
+# coordinates = get_gps_coordinates()
+# print(coordinates['latitude'])
+
+
+# @dataclass
+# class Coordinates:
+#     latitude: float
+#     longitude: float
+#
+#
+# def get_gps_coordinates() -> Coordinates:
+#     """Returns current coordinates using"""
+#     return Coordinates(latitude=10.0, longitude=15.0)
+#
+#
+# coordinates = get_gps_coordinates()
+# print(coordinates.latitude)
