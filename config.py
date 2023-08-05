330d904b638817c539e8cac95a70aa3d8ace4cef
maksim <6234513@gmail.com>
2023-08-05 16:17:48 +0300

    add env and env.example files

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
 USE_ROUNDED_COORDS = False
-OPENWEATHER_API = '909c7b331b35205c8a4b26780af01e47'
+OPENWEATHER_API = env('API_KEY')
 
 OPENWEATHER_URL = (
-    "https://api.openweathermap.org/data/2.5/weather?"
-    "lat={latitude}&lon={longitude}&"
-    "appid=" + OPENWEATHER_API + "&lang=ru&"
-    "units=metric"
+        "https://api.openweathermap.org/data/2.5/weather?"
+        "lat={latitude}&lon={longitude}&"
+        "appid=" + OPENWEATHER_API + "&lang=ru&"
+                                     "units=metric"
 )
-
